6182124c4d458c3289af18f28752c3888b4f7fd3
CodingKitten <121097932+CodingKitten-YT@users.noreply.github.com>
2024-08-29 12:19:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,17 @@
 self.addEventListener('install', function(e) {
-    console.log('install');
-    e.waitUntil(
-      caches.open('kittenstore').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          'https://unpkg.com/onsenui/css/onsenui.min.css',
-          'https://unpkg.com/onsenui/css/onsen-css-components.min.css',
-          'https://unpkg.com/onsenui/js/onsenui.min.js'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(e) {
-    e.respondWith(
-      caches.match(e.request)
-        .then(response => response || fetch(e.request))
-    );
-  });
+  console.log('Service Worker installing.');
+  e.waitUntil(
+    caches.open('kittenstore').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        'https://unpkg.com/onsenui/css/onsenui.min.css',
+        'https://unpkg.com/onsenui/css/onsen-css-components.min.css',
+        'https://unpkg.com/onsenui/js/onsenui.min.js'
+      ]).catch(error => {
+        console.error('Caching failed:', error);
+      });
+    })
+  );
+});
